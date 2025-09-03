2e2dac2df9ed7dc41549a6eb90198156c1b3364d
KubaWalasek <kubariddick@gmail.com>
2025-09-03 13:50:25 +0200

    migracje na postgres

--- Vertigo_Bee_Garden/settings.py
+++ Vertigo_Bee_Garden/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
+import dj_database_url
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -85,11 +86,11 @@ WSGI_APPLICATION = 'Vertigo_Bee_Garden.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    )
 }
+
 
 
 # Password validation
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 set -euxo pipefail
 
 python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+
 
 echo "=== PYTHON/UVICORN DIAGNOSTICS START ==="
 python - <<'PYCODE'
